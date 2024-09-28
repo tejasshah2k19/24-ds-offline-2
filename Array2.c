86c88b5da234da32eb5933e4cbddbd5b42394a31
Tejas Shah <tejasshah2k19@gmail.com>
2024-09-28 18:55:53 +0530

    fun - linear search

--- Array2.c
+++ Array2.c
@@ -4,40 +4,77 @@
 
 int arr[SIZE];
 int i;
+// DS -> complexity ->
 
+// O(N)
 void insert()
 {
-    int data;
-    int location; 
-    
+    int data;     //
+    int location; //
 
     printf("\nEnter data");
-    scanf("%d",&data);//10
+    scanf("%d", &data); // 10
 
     printf("\nEnter location");
-    scanf("%d",&location);
-
-    for(i=SIZE-1; i>location-1;i--){
-        arr[i] = arr[i-1];
+    scanf("%d", &location);
+    // num = 50
+    // location = 1
+    for (i = SIZE - 1; i > location - 1; i--)
+    { // i=1 1>1  Terminate
+        arr[i] = arr[i - 1];
     }
-    arr[location-1] = data; //30 
-
+    arr[location - 1] = data; // 30
+    // count -> 4
+    // location = 1 , loop -> 4
+    // loaction = 2 , loop -> 3
+    //  3 => 2
+    //  4 => 1
+    //  5 => 0
+    //  6 => X
+    // o(1) -> constant  o(n)
 }
 void removeElement()
 {
 }
+
+// O(N)
 void display()
 {
     printf("\nElements in the Array : ");
-    for(i=0;i<SIZE;i++){
-        printf(" %d",arr[i]);
+    for (i = 0; i < SIZE; i++)
+    {
+        printf(" %d", arr[i]);
     }
 }
 
+void linearSearch()
+{
+    int key, flag, i;
+    printf("\nEnter the key that you want to search ::::: ");
+    scanf("%d", &key); // 2
+    flag = 0;
+    for (i = 0; i < SIZE; i++)
+    {
+        if (arr[i] == key)
+        {
+            flag = 1;          // key == a[i] -->
+            printf("\nFound"); //
+            break;
+        }
+    }
+
+    //
+    if (flag == 0)
+    {
+        printf("\n%d Not Found", key);
+    }
+}
 int main()
 {
 
     int choice;
+    int key;
+    int flag;
 
     while (-1)
     {
@@ -59,7 +96,9 @@ int main()
             break;
         case 4:
             exit(0);
-
+        case 5:
+            linearSearch();
+            break;
         default:
             printf("\nInvalid Choice");
             break;
