518af1b47cfb49df76b870c2c3cdc44d2693defa
Tejas Shah <tejasshah2k19@gmail.com>
2025-03-29 18:33:39 +0530

    AVL BF , LL RL RR LR detection

--- AVLTree.c
+++ AVLTree.c
@@ -6,22 +6,43 @@ struct node
     int data;
     struct node *left;
     struct node *right;
-    int height; 
+    int height;
 } *root = NULL;
 
-int calculateHeight(struct node *root){
+int calculateHeight(struct node *root)
+{
     int lh = 0;
-    int rh = 0; 
+    int rh = 0;
 
-    if(root->left != NULL){
+    if (root->left != NULL)
+    {
         lh = root->left->height;
     }
 
-    if(root->right != NULL){
-        rh = root->right->height; 
+    if (root->right != NULL)
+    {
+        rh = root->right->height;
     }
 
-    return lh > rh ? lh+1 : rh+1;
+    return lh > rh ? lh + 1 : rh + 1;
+}
+
+int calculateBF(struct node *root)
+{
+    int lh = 0;
+    int rh = 0;
+
+    if (root->left != NULL)
+    {
+        lh = root->left->height;
+    }
+
+    if (root->right != NULL)
+    {
+        rh = root->right->height;
+    }
+
+    return lh - rh;
 }
 
 struct node *addNode(struct node *root, int num)
@@ -32,7 +53,7 @@ struct node *addNode(struct node *root, int num)
         root->data = num;
         root->left = NULL;
         root->right = NULL;
-        root->height = 1; 
+        root->height = 1;
     }
     else if (num > root->data)
     {
@@ -44,7 +65,34 @@ struct node *addNode(struct node *root, int num)
         root->left = addNode(root->left, num);
     }
 
-    root->height  = calculateHeight(root); 
+    root->height = calculateHeight(root);
+
+    int bf = calculateBF(root);
+
+   
+    
+    if (bf < -1 )
+    {
+        printf("R");
+
+        if(root->right->data < num){
+            printf("R");
+        }else{
+            printf("L");
+        }
+        printf("\n %d %d\n", root->data, bf);
+        
+    }else if(bf>1){
+        printf("L");
+        if(root->left->data < num)
+        {
+            printf("R");
+        }else{
+            printf("L");
+        }
+        printf("\n %d %d\n", root->data, bf);
+    }
+    
 
     return root;
 }
@@ -55,7 +103,7 @@ void inOrder(struct node *root)
     if (root != NULL)
     {
         inOrder(root->left);
-        printf(" %d(%d)", root->data,root->height);
+        printf(" %d(%d)", root->data, root->height);
         inOrder(root->right);
     }
 }
@@ -63,11 +111,28 @@ void inOrder(struct node *root)
 int main()
 {
 
-    root = addNode(root, 30);
-    root = addNode(root, 50);
-    root = addNode(root, 20);
-    root = addNode(root, 10);
+    // RR
+        //  root = addNode(root, 30);
+        // root = addNode(root, 50);
+        // root = addNode(root, 200);
 
-    inOrder(root);
+    // LL
+    //  root = addNode(root, 200);
+    //  root = addNode(root, 50);
+    //  root = addNode(root, 30);
+
+    // LR
+    // root = addNode(root, 200);
+    // root = addNode(root, 30);
+    // root = addNode(root, 50);
+
+
+    // RL
+    // root = addNode(root, 30);
+    // root = addNode(root, 200);
+    // root = addNode(root, 50);
+
+
+    //   inOrder(root);
     return 0;
 }
