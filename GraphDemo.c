859744a7cd2392206dccce8f4ff9a57775d6a363
Tejas Shah <tejasshah2k19@gmail.com>
2025-04-12 19:07:01 +0530

    DFS

--- GraphDemo.c
+++ GraphDemo.c
@@ -7,21 +7,55 @@ struct edge
     int dest; // 20 10 30 20 40 30 10 40
 };
 
-// 20 : 30
+int *visited;
+int totalVisiteCount=0;
+struct edge *e;
+int totalEdges;
+int totalVertex;
+
+int isVisited(int num){
+    int i;
+    for(i=0;i<totalVertex;i++){
+        if(visited[i] == num){
+            return 1;//visited:true 
+        }
+    }
+    return 0;
+}
+
+void dfs(int start){
+    int i;
+    printf(" %d",start);//1 2 
+    visited[totalVisiteCount]=start;
+    totalVisiteCount++;
+    if(totalVisiteCount == totalVertex ){
+        return;
+    }
+    for(i=0;i<totalEdges*2;i++){
+        if(e[i].src == start && isVisited(e[i].dest) == 0 ){
+            start = e[i].dest; 
+            dfs(start);//2             
+        }
+    }
+
+
+}
 
 int main()
 {
 
-    int totalEdges;
-    int totalVertex;
+   
+
     int i;
-    struct edge *e;
+    
     int s, d;
 
     printf("\nEnter total vertex and total edges");
     scanf("%d%d", &totalVertex, &totalEdges);
 
-    e = calloc(8, sizeof(struct edge));
+    visited = calloc(totalVertex,sizeof(int)); //0 0 0 0 0 0 
+
+    e = calloc(totalEdges*2, sizeof(struct edge));
 
     for (i = 0; i < totalEdges * 2; i++)
     {
@@ -37,11 +71,13 @@ int main()
     printf("\nEnter Vertex ? ");
     scanf("%d", &s); // 10
 
-    printf("\n %d is connected with : ",s);
-    for (i = 0; i < totalEdges * 2; i++)
-    {
-        if(e[i].src == s){
-            printf(" %d",e[i].dest);
-        }
-    }
+    // printf("\n %d is connected with : ",s);
+    // for (i = 0; i < totalEdges * 2; i++)
+    // {
+    //     if(e[i].src == s){
+    //         printf(" %d",e[i].dest);
+    //     }
+    // }
+
+    dfs(1);
 }
